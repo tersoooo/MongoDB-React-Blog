1b008559ef93c6f4b3d41434eed02116813a0058
tersoo <latifwork14@gmail.com>
2024-12-14 18:02:36 +0300

    Remove all files from repository

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="container mx-auto mt-5">
-            <p className="text-sm text-center text-gray-400 border-t border-t-[#333] py-5">© 2024 <a href="http://latifaktas.com.tr" className="text-second-color hover:underline" target="_blank">Latifaktas.com.tr</a> Designs Inc. All rights reserved.</p>
-        </div>
-    )
-}
--- src/components/Title.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Title({children}) {
-    return (
-        <h2 className="text-2xl font-Poppins border-l-4 px-2 border-l-second-color font-semibold">{children}</h2>
-    )
-}
